0a6aacedaa8c6a4235117f59dfb070122c3d6dd4
puhrome <phirom.yim@fullsail.edu>
2014-04-13 19:11:51 -0400

    Basic Expressions
    Calculating the year you are born with basic expressions in WebStorm
    using HTML and JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Basic Expressions Created by Phirom Yim on 4/13/14.
+ */
+var yearBorn = 1974;// declaring and defining year born variable
+var age = 2014 - yearBorn; //declaring and defining age expression
+//Current year minus year you're born will equal to your age
+console.log(age);
